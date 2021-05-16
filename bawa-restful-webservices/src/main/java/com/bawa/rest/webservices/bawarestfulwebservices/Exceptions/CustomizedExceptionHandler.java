7710f4de8d16d30bc78acc98e0c7db87279883f5
Harvinder Singh <harvinder.singh@Harvinders-MacBook-Pro.local>
2021-05-16 16:15:58 +0530

    Initial commit

---
+++ bawa-restful-webservices/src/main/java/com/bawa/rest/webservices/bawarestfulwebservices/Exceptions/CustomizedExceptionHandler.java
@@ -0,0 +1,57 @@
+package com.bawa.rest.webservices.bawarestfulwebservices.Exceptions;
+
+import java.util.Date;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.bawa.rest.webservices.bawarestfulwebservices.user.UserNotFoundException;
+
+@RestController
+@ControllerAdvice
+public class CustomizedExceptionHandler extends ResponseEntityExceptionHandler{
+	
+	
+	@ExceptionHandler(Exception.class)
+	public final ResponseEntity<Object> handleAllException(Exception ex, WebRequest request) 
+			throws Exception {
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(ex.getMessage(), new Date(), request.getDescription(false));
+		
+		return new ResponseEntity<Object>(exceptionResponse,HttpStatus.INTERNAL_SERVER_ERROR);
+		
+	}
+		
+
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<Object> handleUserNotFoundException(Exception ex, WebRequest request) 
+			throws Exception {
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(ex.getMessage(), new Date(), request.getDescription(false));
+		
+		return new ResponseEntity<Object>(exceptionResponse,HttpStatus.NOT_FOUND);
+		
+	}
+	
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		ExceptionResponse exceptionResponse = new ExceptionResponse(ex.getMessage(), 
+				new Date(), 
+				ex.getBindingResult().toString());
+		
+		return new ResponseEntity<Object>(exceptionResponse,HttpStatus.BAD_REQUEST);
+	}
+	
+	
+
+}
